c042b3888176d7b831b123575aa59b0ea828071b
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-05-23 13:49:16 +0700

    chore: Refactor App component and Card component, and update UI

--- src/App.js
+++ src/App.js
@@ -1,69 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import Age from "./components/Age";
-import Card from "./components/Card";
 
 function App() {
-  const [age, setAge] = useState(10); // state
-  const hello = "Hello World"; // variable
-
-  const [user, setUser] = useState({
-    id: 1,
-    name: "Vinh Hoang",
-    age: 20,
-    address: "Hanoi",
-  });
-
-  const [usersData, setUsersData] = useState([
-    {
-      id: 1,
-      name: "Vinh Hoang",
-      age: 20,
-      address: "Hanoi",
-    },
-    {
-      id: 2,
-      name: "John Doe",
-      age: 25,
-      address: "New York",
-    },
-    {
-      id: 3,
-      name: "Jane Doe",
-      age: 30,
-      address: "California",
-    },
-  ]);
-
-  const increaseAge = () => {
-    setAge(age + 1);
-  };
-
-  const decreaseAge = () => {
-    setAge(age - 1);
-  };
-
-  const clearUsesData = () => {
-    setUsersData([]);
-  };
-
   return (
     <div className="App">
-      <h1>Age: {age}</h1>
-      <h2>
-        <Age age={age} />
-      </h2>
-      <h3>
-        <Card user={user} />
-      </h3>
-      <p>
-        {usersData.map((user) => (
-          <Card key={user.id} user={user} />
-        ))}
-      </p>
-      <button onClick={increaseAge}>Increase age</button>
-      <button onClick={decreaseAge}>Decrease Age</button>
-      <button onClick={clearUsesData}>Clear UsersData</button>
+      <h1>Form</h1>
       <form>
         <div>
           <label>Username</label>
@@ -73,7 +13,17 @@ function App() {
           <label>Password</label>
           <input type="password" />
         </div>
-        <div className="div">
+        <div>
+          <select>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+          </select>
+        </div>
+        <div>
+          <label>Do you agree with Terms?</label>
+          <input type="checkbox" />
+        </div>
+        <div>
           <button type="submit">Submit</button>
         </div>
       </form>

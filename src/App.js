3d5902c073c559041aeac90d25f5da9207d9cd85
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-05-23 14:53:44 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    setUser((previous) => {
+      console.log(previous);
+      previous = {
+        username: username,
+        password: password,
+      };
+    });
+
+    console.log(user);
+  };
+
+  const handelChangeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Form</h1>
-      <form>
+      <form onSubmit={handleForm}>
         <div>
           <label>Username</label>
-          <input type="text" />
+          <input onChange={handelChangeUsername} type="text" />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" />
+          <input type="password" onChange={handleChangePassword} />
         </div>
         <div>
           <select>
@@ -24,9 +50,14 @@ function App() {
           <input type="checkbox" />
         </div>
         <div>
+          <label>Your age:</label>
+          <input type="number" />
+        </div>
+        <div>
           <button type="submit">Submit</button>
         </div>
       </form>
+      <h2>Form Data</h2>
     </div>
   );
 }

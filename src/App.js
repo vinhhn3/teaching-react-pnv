c25814b5bf5fc921b1c2a5d19d111323c6c036cb
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-05-23 15:44:47 +0700

    chore: Refactor handleForm function, add handleSelect function, and update UI

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import "./App.css";
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
+  const [age, setAge] = useState(0);
+  const [gender, setGender] = useState("female");
   const [user, setUser] = useState({});
 
   const handleForm = (event) => {
     event.preventDefault();
+    if (age < 0) {
+      alert("Age cannot be negative");
+      return;
+    }
     setUser(
       (previous) =>
         (previous = {
           username: username,
           password: password,
+          gender: gender,
+          isSelected: isSelected,
+          age: age,
         })
     );
-
-    console.log(user);
   };
 
   const handelChangeUsername = (event) => {
@@ -27,8 +35,17 @@ function App() {
     setPassword(event.target.value);
   };
 
+  const handleChangeCheckbox = (event) => {
+    setIsSelected(!isSelected);
+  };
+
   const handleSelect = (event) => {
     console.log("select");
+    setGender(event.target.value);
+  };
+
+  const handleChangeAge = (event) => {
+    setAge(event.target.value);
   };
 
   return (
@@ -44,18 +61,18 @@ function App() {
           <input type="password" onChange={handleChangePassword} />
         </div>
         <div>
-          <select onSelect={handleSelect}>
+          <select onChange={handleSelect}>
             <option value="female">Female</option>
             <option value="male">Male</option>
           </select>
         </div>
         <div>
           <label>Do you agree with Terms?</label>
-          <input type="checkbox" />
+          <input type="checkbox" onChange={handleChangeCheckbox} />
         </div>
         <div>
           <label>Your age:</label>
-          <input type="number" />
+          <input type="number" onChange={handleChangeAge} />
         </div>
         <div>
           <button type="submit">Submit</button>
@@ -63,6 +80,10 @@ function App() {
       </form>
       <h2>Form Data</h2>
       <p>Username: {user.username}</p>
+      <p>Password: {user.password}</p>
+      <p>Gender: {user.gender}</p>
+      <p>Is selected: {isSelected ? "Yes" : "No"}</p>
+      <p>Age: {user.age}</p>
     </div>
   );
 }
